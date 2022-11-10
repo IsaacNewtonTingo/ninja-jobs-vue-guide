67c156a8abfa2d0574d44d8db06fba6791594d33
Isaac  Tingo <newtone480@gmail.com>
2022-11-10 15:43:11 +0300

    Ninja-jobs-vue-guide

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore quis
+      inventore tenetur rerum delectus saepe, fuga magnam sed quod, dolorum
+      ducimus nam distinctio mollitia nisi quos voluptatibus. Doloremque culpa
+      excepturi itaque odit at vero eligendi quasi laudantium mollitia eveniet
+      facere natus suscipit, nihil minima quisquam inventore reprehenderit,
+      dicta repudiandae maiores? Quisquam nobis iusto voluptatibus assumenda
+      sequi, non sunt sapiente ad similique, in obcaecati numquam recusandae
+      officia porro inventore nesciunt omnis nam officiis ipsum libero quibusdam
+      quidem alias voluptatum delectus? Ea qui veritatis eos labore nulla. Odit
+      rem nemo, at delectus autem quam exercitationem. Ad odit earum minima.
+      Laboriosam, dolorum quidem!
+    </p>
+
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit
+      quaerat ipsam saepe labore at consequatur doloremque dolor quasi
+      consequuntur iusto, iure ea fugit veniam ipsa optio dignissimos
+      perferendis alias distinctio hic laborum autem tenetur! Dicta porro quas
+      fugit magnam, maxime odio voluptate unde, facere culpa corporis sed
+      tenetur ab voluptates doloremque deserunt voluptatibus sapiente.
+      Distinctio ipsa voluptates, sequi nihil corrupti eveniet aut error
+      voluptatibus minus, totam aspernatur accusamus. Corrupti animi dolor
+      mollitia minus est quisquam quod labore nobis! Sapiente ipsam, tempora
+      illo tempore, ducimus quae est voluptas delectus nobis libero voluptatibus
+      rem eum iste fuga inventore eos provident numquam, ex ratione accusamus
+      tenetur perspiciatis. Officiis iure, nemo cum nihil minus ut nesciunt,
+      aspernatur debitis eligendi ex explicabo quam dolorum id delectus
+      inventore quisquam! Nisi in doloribus, molestiae illo, distinctio odit
+      nesciunt corporis autem similique quis quisquam? Perspiciatis, earum ipsam
+      voluptate amet molestias sint incidunt beatae temporibus tenetur cumque
+      adipisci distinctio repudiandae neque explicabo voluptas at fugit numquam
+      iure aperiam facere voluptatibus harum consequuntur. Dolore repellendus
+      voluptatem aliquid nihil recusandae quos optio eaque error tempora velit
+      nemo alias porro, nobis earum, dolorum doloribus? Nesciunt nisi eligendi
+      aut. Ducimus voluptatum animi eveniet atque. Aliquid repellendus accusamus
+      molestiae quod necessitatibus qui atque corporis!
+    </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"></div>
+  <h1>Home page</h1>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero illum, iure
+    aliquid deleniti vel totam! Veniam commodi molestiae, atque ratione
+    similique provident! Minima possimus laudantium quaerat itaque, porro
+    voluptatum tempora libero. Corrupti, necessitatibus. Maiores amet ab ullam
+    corrupti rem, nisi necessitatibus! Voluptates tempore animi beatae quaerat
+    mollitia maxime asperiores minus, debitis quae rerum temporibus iste earum
+    voluptas! Eligendi ea, soluta alias neque consequatur molestiae tenetur quae
+    enim ex suscipit nostrum repudiandae facilis sed a esse amet repellat sequi
+    possimus impedit! Quaerat voluptatibus porro dolor! Magni, itaque?
+    Consectetur iure esse magni, placeat excepturi autem doloribus quidem
+    commodi consequuntur, accusamus dolorum deserunt. Quasi officia nisi eius
+    recusandae consequuntur iusto cumque possimus maxime eaque est nostrum
+    numquam deserunt eum consequatur odio, nesciunt, quia provident molestias
+    exercitationem dicta libero? Hic, maxime enim ut iste tempore voluptates
+    alias dolore quae placeat ipsum ratione? Officiis blanditiis commodi harum
+    laudantium odit maiores perspiciatis neque minima adipisci possimus
+    accusamus quidem, sit natus inventore voluptates incidunt expedita
+    distinctio eum alias atque at quasi fuga? Veniam fugit assumenda voluptate
+    modi aliquid. Repellat pariatur esse reiciendis quo voluptate minus. Error
+    tempore, mollitia porro animi dolorem assumenda quis recusandae inventore
+    atque, veritatis iusto. Iusto exercitationem sequi veniam, asperiores
+    provident molestias voluptates accusamus.
+  </p>
+
+  <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus nisi ipsam
+    nemo sint facilis. Suscipit doloremque eum omnis quod rerum, neque quisquam
+    quibusdam aut consectetur harum animi vero aliquid asperiores nam placeat
+    dicta iusto ullam perspiciatis in modi magni illum cum optio? Tempora
+    corrupti quo ea delectus rem, ratione dolores magnam omnis laudantium optio
+    soluta labore deserunt dignissimos obcaecati? Iusto consequatur numquam
+    illum incidunt ea natus minus saepe molestias nobis expedita! Cumque, quo
+    illum sequi aut nesciunt ex quas explicabo, molestias nostrum praesentium
+    reiciendis earum sed officia a repellendus quos porro odio sunt voluptate
+    tenetur! Officia quasi quaerat nihil vitae quis, sint at atque voluptas
+    libero iste aliquid quas natus ipsam dolorum corporis ad corrupti laborum
+    perspiciatis et saepe. Animi architecto voluptatem asperiores impedit unde
+    odio fuga incidunt aut, delectus deserunt voluptates repellendus. Veritatis
+    rem dicta deleniti velit quibusdam totam, consequuntur mollitia perferendis
+    atque eos inventore ipsam voluptates quo illo?
+  </p>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
